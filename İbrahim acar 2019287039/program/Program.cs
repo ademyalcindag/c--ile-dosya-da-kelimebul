d62cb82490f3972663bab63fb4d286927c5c069a
ademyalcindag <ademyalcindag2@gmail.com>
2022-05-18 10:13:47 +0300

    c# iledosyada kelimebul
    dosyada olan kelimeleri bul

---
+++ İbrahim acar 2019287039/program/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Program
+    {
+        
+        
+        
+            static void Main(string[] args)
+            {
+                
+                List<string> klmlr =System.IO.File.ReadAllLines("dosyam.txt").ToList();
+                
+                while (true)
+                {
+                    Console.WriteLine("Bir KELİME girin.:");
+                    string kelime= Console.ReadLine();
+                    if (kelime.ToLower() == "çık")
+                        break;
+                    if (klmlr.Contains(kelime))
+                    {
+                        int statirNO = klmlr.IndexOf(kelime) + 1;
+                        Console.WriteLine("Girdiğin '" + kelime + " kelimesi  text dosyasında " + statirNO.ToString() + "bulundu\n");
+                    }
+                    else
+                        Console.WriteLine("Girdiğiniz '" + kelime + "' kelime dosyada bulunamadı. Tekrar deneyin\n");
+                }
+
+            }
+        }
+    }
+    
+
